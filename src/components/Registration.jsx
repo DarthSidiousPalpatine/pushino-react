321b6d4fac7e5d2c1223c372c32145d02779c5a8
Darth Sidious <andrgysev02@mail.ru>
2026-03-05 05:40:02 +0400

    Сделал модальное окно первое, осталось ещё 4

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,19 +1,90 @@
+
+import React, { useState } from 'react';
 import '../styles/registration.css'
+import { Modal } from '@mui/material';
 
 function Registration() {
+    const [formData, setFormData] = useState({
+        name: '',
+        mail: '',
+        phone: '',
+        destination: '',
+        agree: ''
+    });
+    const [errors, setErrors] = useState({});
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }));
+
+        if (errors[name]) {
+            setErrors(prev => ({ ...prev, [name]: '' }));
+        }
+    };
+
+    const validateForm = () => {
+        const newErrors = {};
+
+        if (!formData.name) {
+            newErrors.name = 'Имя обязательно';
+        }
+
+        if (!formData.mail) {
+            newErrors.mail = 'E-mail обязателен';
+        } else if (!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.mail))) {
+            newErrors.mail = 'Некорректный e-mail';
+        }
+
+        if (!formData.phone) {
+            newErrors.phone = 'Телефон обязателен';
+        } else if (!(/^\+7|8\d{10}$/.test(formData.phone))) {
+            newErrors.phone = 'Некорректный номер';
+        }
+
+        if (!formData.agree) {
+            newErrors.agree = 'Нужно согласие';
+        }
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (validateForm()) {
+            setIsModalOpen(true);
+            e.target.reset();
+        }
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+        setFormData({ username: '', email: '', password: '', agree: false });
+    };
+
     return (
         <div className="block__WithHeader" id="Registration">
             <span className="block__WidthHeader__Header">ЗАПИШИТЕСЬ НА ТУР В ПУЩИНО<br/><span className="block__WithHeader__SmallHeader">мы свяжемся с вами для уточнения деталей</span></span>
-            <div className="Registration__body">
+            <form onSubmit={handleSubmit} className="Registration__body">
                 <div className="Registration__fields">
-                    <input name="Enter Name" className="Registration__inputField" type="text" required={true} placeholder="Ваше имя"/>
-                    <input name="Enter Email" className="Registration__inputField" type="email" required={true} placeholder="Ваш e-mail"/>
-                    <input name="Enter Telephone" className="Registration__inputField" type="tel" required={true} placeholder="Ваш номер телефона"/>
-                    <input name="Enter Tour" className="Registration__inputField" type="text" required={true} placeholder="Понравившийся вам маршрут"/>
-                    <div className="Registration__inputCheck"><input name="Agree" className="Registration__checkBox" type="checkbox" required={true}/> <span>Я даю согласие на обработку моих персональных данных</span></div>
+                    <input onChange={handleChange} name="name" className="Registration__inputField" type="text" required={true} placeholder="Ваше имя"/>
+                    <input onChange={handleChange} name="mail" className="Registration__inputField" type="email" required={true} placeholder="Ваш e-mail"/>
+                    <input onChange={handleChange} name="phone" className="Registration__inputField" type="tel" required={true} placeholder="Ваш номер телефона"/>
+                    <input onChange={handleChange} name="destination" className="Registration__inputField" type="text" required={true} placeholder="Понравившийся вам маршрут"/>
+                    <div className="Registration__inputCheck"><input onChange={handleChange} name="agree" className="Registration__checkBox" type="checkbox" required={true}/> <span>Я даю согласие на обработку моих персональных данных</span></div>
                 </div>
                 <input name="Submit Button" className="banner__Btn" type="submit" value="Оставить заявку"/>
-            </div>
+            </form>
+            <Modal open={isModalOpen} onClose={handleCloseModal} aria-labelledby="registration-success-modal" aria-describedby="modal-success-message">
+                <div className="Registration__modal">
+                    <p>Спасибо за вашу заявку, <span className="accentText">в скором времени мы свяжемся с вами</span> для уточнения деталей поездки.</p>
+                </div>
+            </Modal>
         </div>
     );
 }
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -41,37 +41,38 @@ function Tours() {
                     <button data-value={2} className={chosenTour==="2" ? "Tours__SelectTour_Button --active" : "Tours__SelectTour_Button"} onClick={chooseTour}>Архитектурный</button>
                     <button style={{borderRightStyle: "none"}} data-value={3} className={chosenTour==="3" ? "Tours__SelectTour_Button --active" : "Tours__SelectTour_Button"} onClick={chooseTour}>Астрономический</button>
                 </div>
+                <div className="Tours__Main">
+                    {picturesData && Slider(picturesData)}
 
-                {picturesData && Slider(picturesData)}
-
-                <div className="Tours__Information">
-                    <div className="Tours__Information_Block">
-                        <p className={showText ? "Tours__Information_Text --fullText" : "Tours__Information_Text"}>
-                            {textData && textData.map((paragraph, index) =>
-                                <span key={index}>{paragraph}<br/></span>
-                            )}
-                        </p>
-                        <div className="Tours__Information_Icons">
-                            {iconsData && iconsData.map((icon, index) =>
-                                <div key={index} className="icon__block">
-                                    <div className="icon__icon">
-                                        <img src={require(`../icons/${icon.icon}`)} alt={icon.iconName}/>
+                    <div className="Tours__Information">
+                        <div className="Tours__Information_Block">
+                            <p className={showText ? "Tours__Information_Text --fullText" : "Tours__Information_Text"}>
+                                {textData && textData.map((paragraph, index) =>
+                                    <span key={index}>{paragraph}<br/></span>
+                                )}
+                            </p>
+                            <div className="Tours__Information_Icons">
+                                {iconsData && iconsData.map((icon, index) =>
+                                    <div key={index} className="icon__block">
+                                        <div className="icon__icon">
+                                            <img src={require(`../icons/${icon.icon}`)} alt={icon.iconName}/>
+                                        </div>
+                                        {icon.text.map((line, index2) =>
+                                            <span key={index2} className={line.accent ? "accentText" : "icon__text"}>{line.text}</span>
+                                        )}
                                     </div>
-                                    {icon.text.map((line, index2) =>
-                                        <span key={index2} className={line.accent ? "accentText" : "icon__text"}>{line.text}</span>
-                                    )}
-                                </div>
-                            )}
+                                )}
+                            </div>
                         </div>
+                        <button className="Tours__ShowMore_Button" onClick={toggleText}>{showText ? "⯅" : "подробнее"}<br/>{showText ? "свернуть": "⯆"}</button>
                     </div>
-                    <button className="Tours__ShowMore_Button" onClick={toggleText}>{showText ? "⯅" : "подробнее"}<br/>{showText ? "свернуть": "⯆"}</button>
-                </div>
 
-                <div className="Tours__Map">
-                    <YMap location={reactify.useDefault(LOCATION)}>
-                        <YMapDefaultSchemeLayer />
-                        <YMapDefaultFeaturesLayer />
-                    </YMap>
+                    <div className="Tours__Map">
+                        <YMap location={reactify.useDefault(LOCATION)}>
+                            <YMapDefaultSchemeLayer />
+                            <YMapDefaultFeaturesLayer />
+                        </YMap>
+                    </div>
                 </div>
             </div>
         </div>
